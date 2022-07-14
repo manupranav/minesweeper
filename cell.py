cb56f1de1f9a53ce8688e621b9bf8deb3d8ad5f1
manupranav <62414791+manupranav@users.noreply.github.com>
2022-07-13 18:01:30 -0700

    first commit

---
+++ cell.py
@@ -0,0 +1,22 @@
+from tkinter import Button
+
+
+class Cell:
+    def __init__(self, is_mine=False):
+        self.is_mine = is_mine
+        self.cell_btn_object = None
+
+    def create_btn_object(self, location):
+        btn = Button(location, text="Text", width=12, height=4)
+        btn.bind("<Button-1>", self.left_click_action)
+        btn.bind("<Button-3>", self.right_click_action)
+
+        self.cell_btn_object = btn
+
+    def left_click_action(self, event):
+        print(event)
+        print("I am left clicked !")
+
+    def right_click_action(self, event):
+        print(event)
+        print("I am right clicked !")
